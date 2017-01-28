4d5601cd6e44667b346ae9c8ca632934e4bfd0a1
chrisbacon <bacon.chris@gmail.com>
2017-01-28 15:02:04 +0000

    fixed issue with reset button causing frame rate speed up

--- public/app.js
+++ public/app.js
@@ -3,41 +3,52 @@ var startingPoint = new Point(30, 30)
 var length = 40;
 var fps = 10;
 
+var view;
+var snake;
+var game;
+
+var frameID;
+
+var init = function() {
+    view = new View(canvas, segmentSize);;
+    snake = new Snake(startingPoint, length, segmentSize);
+    game = new Game(snake, canvas.width, canvas.height);
+}
+
+var draw = function() {
+    setTimeout(function() {
+        view.draw(snake);
+        snake.setDirection(view.getDirection());
+        snake.move();
+        game.checkForCrossing();
+        game.checkInBounds();
+        if (game.running) {
+            frameID = window.requestAnimationFrame(draw)
+            console.log(frameID, snake.position[0].x)
+        }
+    }, 1000 / fps);
+};
+
 
 window.onload = function() {
     canvas = document.querySelector('canvas');
-    var view = new View(canvas, segmentSize);
-    var snake = new Snake(startingPoint, length, segmentSize);
     var button = document.querySelector('button');
-    var game = new Game(snake, canvas.width, canvas.height);
-
-    var draw = function() {
-        setTimeout(function() {
-            view.draw(snake);
-            snake.setDirection(view.getDirection());
-            snake.move();
-            game.checkForCrossing();
-            game.checkInBounds();
-            if (game.running) {
-                window.requestAnimationFrame(draw)
-            }
-        }, 1000 / fps);
-    };
+    init();
 
     canvas.onkeydown = function(event) {
         view.handleKeyPress(event.key);
     };
 
-
     button.onclick = function() {
-        view.reset();
-        snake = new Snake(startingPoint, length, segmentSize);
-        game = new Game(snake, canvas.width, canvas.height);
-        canvas.focus();
+        game.running = false;
+        setTimeout(function() {
+            init();
+            canvas.focus();
+        }, 100);
     };
 
     canvas.onfocus = function() {
-        window.requestAnimationFrame(draw);
+        draw();
     }
 
     canvas.focus();
